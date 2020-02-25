3750b9566af112fd7c5742df9271146d8585c96d
Pablo Garcia Andres <p.garciaandres@douglas.de>
2020-02-25 11:27:40 +0100

    Server

--- client/src/services/tournamentService.js
+++ client/src/services/tournamentService.js
@@ -2,22 +2,22 @@ import axios from 'axios';
 
 export default {
     getTournaments: async() => {
-        let res = await axios.get('http://localhost:5000/api/getTournaments');
+        let res = await axios.get('/api/getTournaments');
         return res.data;
     },
 
     updateTournament: async (id, tournament) => {
-        let res = await axios.put('http://localhost:5000/api/updateTournament/' + id, tournament);
+        let res = await axios.put('/api/updateTournament/' + id, tournament);
         return res.data;
     },
 
     createTournament: async (tournament) => {
-        let res = await axios.post('http://localhost:5000/api/createTournament/', tournament);
+        let res = await axios.post('/api/createTournament/', tournament);
         return res.data;
     },
 
     deleteTournament: async (id) => {
-        let res = await axios.post('http://localhost:5000/api/deleteTournament/' + id);
+        let res = await axios.post('/api/deleteTournament/' + id);
         return res.data;
     }
 }
